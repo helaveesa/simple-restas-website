573002d2c571e1cb930a1906aa9ac9430b4b7038
helaveesa <helaveesa.nolder@gmail.com>
2012-09-07 18:46:02 +0400

    modified init.lisp

--- init.lisp
+++ init.lisp
@@ -1,5 +1,12 @@
 (in-package #:srw)
 
+(defun menu ()
+  (list (list :link "/" :title "Главная")
+        (list :link "/history" :title "История")
+        (list :link "/staff" :title "Сотрудники")
+        (list :link "/foto/" :title "Фотографии")
+        (list :link "/contacts" :title "Контакты"))))
+
 ;; start
 (restas:start '#:srw #|:hostname "rigidus.ru"|# :port 9333)
 (restas:debug-mode-on)
