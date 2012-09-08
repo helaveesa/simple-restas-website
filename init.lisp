1e065f94b5c62975ac88f0074480d91bdbdaf396
helaveesa <helaveesa.nolder@gmail.com>
2012-09-08 14:37:54 +0400

    modified_contacts_file

--- init.lisp
+++ init.lisp
@@ -5,7 +5,7 @@
         (list :link "/history" :title "История")
         (list :link "/staff" :title "Сотрудники")
         (list :link "/foto/" :title "Фотографии")
-        (list :link "/contacts" :title "Контакты"))))
+        (list :link "/contacts" :title "Контакты")))
 
 ;; start
 (restas:start '#:srw #|:hostname "rigidus.ru"|# :port 9333)
